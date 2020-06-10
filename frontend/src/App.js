f33f6b1427f7446669ac5f45c843023a1f468324
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-10 17:54:22 -0300

    feat: create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <div class="grid-container">
+      <header class="header">
+        <div class="brand">
+          <button onClick={openMenu}>
+            &#9776;
+        </button>
+          <a href="index.html">Talassa</a>
+          <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link>
+        </div>
+        <div class="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <aside class="sidebar">
+        <h3>Shopping Categories</h3>
+        <button class="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+
+        </ul>
+      </aside>
+      <main class="main">
+        <div class="content">
+          <ul class="products">
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div class="product">
+                <img class="product-image" src="/images/d1.png" alt="product" />
+                <div class="product-name">
+                  <a href="product.html">Air Max 270 React Masculino</a>
+                </div>
+                <div class="product-brand">Nike</div>
+                <div class="product-price">$300</div>
+                <div class="product-rating">4.8 start (127 Reviews)</div>
+              </div>
+            </li>
+
+          </ul>
+        </div>
+
+      </main>
+      <footer class="footer">
+        All right reserved.
+    </footer>
+    </div>
+  );
+}
+
+export default App;
